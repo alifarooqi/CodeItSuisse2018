155795b398a20fa93b8bc9c9cd827d1969eb889c
Abhimanyue Singh Tanwar <abhimanyu891998@users.noreply.github.com>
2018-09-22 17:17:17 +0800

    deeplearning added

---
+++ routes/deeplearning.js
@@ -0,0 +1,29 @@
+import { Router } from "express";
+import axios from 'axios'
+import { isPrimitive } from "util";
+import { exists } from "fs";
+var math = require('mathjs');
+var router = Router();
+
+
+router.post('/', function (req, res, next) {
+    var input = req.body.input;
+    var output = req.body.output;
+    var question = req.body.question;
+
+
+    var a = input;
+    var asquare = [a[0],a[1],a[2]];
+    var b = output;
+    var bsquare = [b[0],b[1],b[2]];
+    var ainv = math.inv(asquare);
+    var ans = math.multiply(ainv,bsquare);
+    var finalans = question[0]*ans[0] + question[1]*ans[1] + question[2]*ans[2];
+    var jsonans = {
+        "answer" : finalans
+    }
+    res.send(JSON.stringify(jsonans));
+});
+
+
+export default router;
