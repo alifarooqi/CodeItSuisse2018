3e8cc500fab2bea1171c5754352621fdd9a893b5
Abhimanyue Singh Tanwar <abhimanyu891998@users.noreply.github.com>
2018-09-22 09:53:25 +0800

    Team name added

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Team-X' });
+  res.render('index', { title: 'Team-SegFault' });
 });
 
 module.exports = router;
