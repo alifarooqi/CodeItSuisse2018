56dbf0ebbef2fb018d4701b1d494445394d14185
Muhammad Ali Farooqi <m.ali_farooqi@hotmail.com>
2018-09-23 09:49:19 +0800

    Training once

--- routes/deeplearning.js
+++ routes/deeplearning.js
@@ -6,6 +6,7 @@ var math = require('mathjs');
 var router = Router();
 const synaptic = require('synaptic');
 var trainedData = require('../traineddata.json')
+const myNetwork = synaptic.Network.fromJSON(trainedData);
 
 
 router.post('/question-1', function (req, res, next) {
@@ -34,8 +35,6 @@ router.post('/question-1', function (req, res, next) {
 
 router.post('/question-2', function (req, res, next){
     var testSet = req.body.question;
-    const myNetwork = synaptic.Network.fromJSON(trainedData);
-
     var output = []
     testSet.forEach((test, idx)=>{
         var result = myNetwork.activate(test)
